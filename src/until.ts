bce31461990237ce2c9413e4e8daee20262c35e9
Artem Zakharchenko <kettanaito@gmail.com>
2022-02-16 16:30:14 +0100

    feat: return discriminated union object

--- src/until.ts
+++ src/until.ts
@@ -3,13 +3,17 @@
  * @example
  * const [error, data] = await until(() => asyncAction())
  */
-export const until = async <DataType = unknown, ErrorType = Error>(promise: () => Promise<DataType>): Promise<[ErrorType, DataType]> => {
+export const until = async <DataType = unknown, ErrorType = Error>(
+  promise: () => Promise<DataType>,
+): Promise<
+  { error: ErrorType; data: null } | { error: null; data: DataType }
+> => {
   try {
     const data = await promise().catch((error) => {
       throw error
     })
-    return [null, data]
+    return { error: null, data }
   } catch (error) {
-    return [error, null]
+    return { error, data: null }
   }
 }
