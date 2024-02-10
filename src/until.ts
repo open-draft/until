efee9708ed6d801ffdf3ea19f487535b73437669
singhAmandeep007 <amandeeps0803@gmail.com>
2024-02-10 21:28:55 +0530

    chore: Bump typescript verision to support type safe destructuring of return value.

--- src/until.ts
+++ src/until.ts
@@ -10,8 +10,11 @@ export type AsyncTuple<
 
 /**
  * Gracefully handles a given Promise factory.
+ * @template ErrorType The type of the error promise may throw.
+ * @template DataType The type of the data promise may return.
+ * @param promise function that returns a promise.
  * @example
- * const { error, data } = await until(() => asyncAction())
+ * const { error, data } = await until<UserFetchError, User>(() => fetchUser(id))
  */
 export const until = async <
   ErrorType extends any = Error,
@@ -20,11 +23,11 @@ export const until = async <
   promise: () => Promise<DataType>,
 ): Promise<AsyncTuple<ErrorType, DataType>> => {
   try {
-    const data = await promise().catch((error) => {
+    const data = await promise().catch((error: ErrorType) => {
       throw error
     })
     return { error: null, data }
   } catch (error) {
-    return { error, data: null }
+    return { error: error as ErrorType, data: null }
   }
 }
